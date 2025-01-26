4ad08c89c9276898757a16795d80ab52f7711ec2
kafle1 <kafleniraj@gmail.com>
2025-01-26 23:15:30 +0545

    feat: Add reusable UI components with shadcn/ui integration

---
+++ src/lib/components/ui/avatar/avatar-image.svelte
@@ -0,0 +1,15 @@
+<!-- src/lib/components/ui/avatar/avatar-image.svelte -->
+<script lang="ts">
+    import { Avatar as AvatarPrimitive } from "bits-ui";
+    import { cn } from "$lib/utils";
+
+    type $$Props = AvatarPrimitive.ImageProps;
+
+    let className: $$Props["class"] = undefined;
+    export { className as class };
+</script>
+
+<AvatarPrimitive.Image
+    class={cn("aspect-square h-full w-full", className)}
+    {...$$restProps}
+/> 
